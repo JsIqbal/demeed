6eef5929a95cabee2215b5434ad665265d846ab9
Iqbal Hossain <110350034+JsIqbal@users.noreply.github.com>
2022-10-08 18:41:56 +0600

    Iqbal/features/finalized v1 (#2)
    * Finalize the Project-v1

--- src/config/lib/sequelize.js
+++ src/config/lib/sequelize.js
@@ -1,10 +1,21 @@
-const { Sequelize } = require('sequelize');
+const path = require("path");
+const { Sequelize } = require("sequelize");
+const nodeCache = require(path.join(process.cwd(), "src/config/lib/nodecache"));
 
-const sequelize = new Sequelize('project', 'root', '', {
-    host: 'localhost',
-    dialect: "mysql",
-    logging: false,
-    sync: true
-})
+const DB_HOST = nodeCache.getValue("DB_HOST");
+const DB_USER = nodeCache.getValue("DB_USER");
+const DB_PASSWORD = nodeCache.getValue("DB_PASSWORD");
+
+// console.log('------------------------------------------', DB_HOST)
+// console.log('------------------------------------------', DB_USER)
+// console.log('------------------------------------------', DB_PASSWORD)
+
+const sequelize = new Sequelize("blog", DB_USER, DB_PASSWORD, {
+	host: DB_HOST,
+	// port: 3030,
+	dialect: "mysql",
+	logging: false,
+	sync: true,
+});
 
 module.exports = sequelize;
--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,114 +1,116 @@
-const jwt = require('jsonwebtoken');
-const User = require('./user.model');
-const registerSchema = require('./user.schema');
+const path = require("path");
+const User = require("./user.model");
+const { generateAccessToken } = require("./user.service");
+const EmailService = require("../../config/lib/email-service/email.service");
 
-const users = async (req, res) => {
-    try {
-        const users = await User.findAll();
-        res.status(200).send(users);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error.");
-    }
-}
-
-const signup = async (req, res) => {
+const login = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        try {
-            const [ user, created ] = await User.findOrCreate(
-                {
-                    where: { email },
-                    defaults: { email, password }
-                }
-            )
-            if (!created) {
-                res.status(409).send("Already Exists");
-            }
-            else {
-                res.status(201).send(user);
-            }
-        }
-        catch (err) {
-            return res.status(500).send("Internal Server error.");
-        }
-    }
-    catch (err) {
-        res.status(500).send("Internal Server error.");
+        const user = await User.findOne({ where: { email, password } });
+
+        if (!user) {
+            return res.status(400).send("Invalid credentials.");
+        };
+
+        res.cookie("access_token", generateAccessToken(user), {
+            httpOnly: true,
+            signed: true
+        });
+
+        res.status(200).send(user);
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).send("Internal server error.");
     }
 }
 
-const login = async (req, res) => {
-    const { email, password } = req.body;
-
+const signUp = async (req, res) => {
     try {
-        const user = await User.findOne(
-            {
-                where: {
-                    email,
-                    password
-                }
-            }
-        )
-    
-        if (user) {
-            const access_token = jwt.sign(
-                {
-                    id: user.id
-                },
-                "jwt-secret",
-                {
-                    expiresIn: "1h",
-                    issuer: user.id.toString()
-                }
-            );
+        const { email, password } = req.body;
 
-            res.cookie("access_token", access_token, { httpOnly: true, signed: true });
-            res.status(200).send(user);
-        }
-        else {
-            res.status(404).send("Not Found");
-        }
-    }
-    catch (err) {
-        res.status(500).send("Internal Server Error");
-    }
-}
+        const [ user, created ] = await User.findOrCreate({
+            where: { email },
+            defaults: { email, password }
+        });
 
-const profile = async (req, res) => {
-    const token = req.signedCookies["access_token"];
+        if(!created) return res.status(409).send("User is already created.");
 
-    if (!token) {
-        res.status(400).send("Bad Request.");
-    }
-    else {
-        const payload = jwt.verify(token, "jwt-secret");
-        const { id } = payload;
+		const options = {
+			toAddresses: [email],
+			templateUrl: "src/config/lib/email-service/templates/email.handlebars",
+			subject: "Registration Confirmation",
+			data: {},
+		};
 
-        try {
-            const user = await User.findOne(
-                {
-                    where: { id }
-                }
-            )
+		EmailService.send(options);
 
-            res.status(200).json(user);
-        }
-        catch (err) {
-            res.status(404).send("User Not Found.");
-        }
-    }
-}
+        return res.status(201).send(user);
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).send("Internal server error.")
+    };
+};
 
 const logout = (req, res) => {
-    res.clearCookie("access_token");
-    res.send("Logged Out");
-}
+	res.clearCookie("access_token");
+	res.send("Logged out.");
+};
 
-module.exports.users = users;
-module.exports.signup = signup;
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.findAll();
+
+        res.status(200).send(users);
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).send("Internal server error.")
+    };
+};
+
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { password } = req.body;
+
+        const user = await User.findOne({ where: { id } });
+
+        if (!user) return res.status(409).send("User not found!");
+        
+        if (password) await user.update({ password });
+
+        return res.status(201).send(user);
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).send("Internal server error.")
+    };
+};
+
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findOne({ where: { id } });
+
+        if (!user) return res.status(409).send("User not found!");
+
+        await User.destroy({ where: { id } });
+
+        return res.status(201).send(user);
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).send("Internal server error.")
+    };
+};
+
 module.exports.login = login;
-module.exports.profile = profile;
 module.exports.logout = logout;
+module.exports.signUp = signUp;
+module.exports.getUsers = getUsers;
+module.exports.updateUser = updateUser;
+module.exports.deleteUser = deleteUser;
